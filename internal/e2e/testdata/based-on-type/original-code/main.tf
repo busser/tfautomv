e05745549fdffa71644cc02f423942e3f70656d9
Arthur Busser <arthur.busser@gmail.com>
2022-08-02 03:55:27 +0200

    refactor: remove unnecessary requirements

--- internal/e2e/testdata/based-on-type/original-code/main.tf
+++ internal/e2e/testdata/based-on-type/original-code/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.3.2"
-    }
-  }
-}
-
 resource "random_pet" "original" {}
 
 resource "random_id" "original" {
